d25376591cd416cc3920415aa7eb1e40c9ba250b
Putri Maharani <maharani2005.putrii@gmail.com>
2025-05-07 05:43:32 +0700

    Membuat tombol FAB

--- app/src/main/java/com/putrimaharani0087/miniproject2/screen/MainScreen.kt
+++ app/src/main/java/com/putrimaharani0087/miniproject2/screen/MainScreen.kt
@@ -5,15 +5,20 @@ import android.widget.Toast
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Add
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.FloatingActionButton
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBar
@@ -37,6 +42,8 @@ import com.putrimaharani0087.miniproject2.ui.theme.MiniProject2Theme
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainScreen() {
+    val context = LocalContext.current
+
     Scaffold(
         topBar = {
             TopAppBar(
@@ -48,6 +55,20 @@ fun MainScreen() {
                     titleContentColor = Color.Black
                 )
             )
+        },
+        floatingActionButton = {
+            FloatingActionButton(
+                onClick = {
+                    Toast.makeText(context, R.string.belum_bisa, Toast.LENGTH_SHORT).show()
+                },
+                containerColor = Color.White
+            ) {
+                Icon(
+                    imageVector = Icons.Filled.Add,
+                    contentDescription = stringResource(R.string.tambah_task),
+                    tint = Color.Black
+                )
+            }
         }
     ) { innerpadding ->
         ScreenContent(Modifier.padding(innerpadding))
@@ -76,7 +97,8 @@ fun ScreenContent(modifier: Modifier = Modifier) {
     }
     else {
         LazyColumn(
-            modifier = modifier.fillMaxSize()
+            modifier = modifier.fillMaxSize(),
+            contentPadding = PaddingValues(bottom = 84.dp)
         ) {
             items(data) {
                 ListItem(task = it) {
