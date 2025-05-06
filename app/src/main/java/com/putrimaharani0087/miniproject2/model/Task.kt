1f97ffeae130d598a4c35e3f6cb903dc514a0e35
Putri Maharani <maharani2005.putrii@gmail.com>
2025-05-07 04:17:22 +0700

    Menyiapkan data di ViewModel

---
+++ app/src/main/java/com/putrimaharani0087/miniproject2/model/Task.kt
@@ -0,0 +1,10 @@
+package com.putrimaharani0087.miniproject2.model
+
+import java.util.Date
+
+data class Task(
+    val id: Long,
+    val judul: String,
+    val deskripsi: String,
+    val deadline: String
+)
---
+++ app/src/main/java/com/putrimaharani0087/miniproject2/screen/MainViewModel.kt
@@ -0,0 +1,51 @@
+package com.putrimaharani0087.miniproject2.screen
+
+import androidx.lifecycle.ViewModel
+import com.putrimaharani0087.miniproject2.model.Task
+
+class MainViewModel : ViewModel() {
+    val data = listOf(
+        Task(
+            1,
+            "Mengerjakan TP Mobpro",
+            "kerjakan sebelum jam praktikum",
+            "2025-05-21 23:59:59"
+        ),
+        Task(
+            2,
+            "Mengerjakan TP Mobpro",
+            "kerjakan sebelum jam praktikum",
+            "2025-05-21 23:59:59"
+        ),
+        Task(
+            3,
+            "Mengerjakan TP Mobpro",
+            "kerjakan sebelum jam praktikum",
+            "2025-05-21 23:59:59"
+        ),
+        Task(
+            4,
+            "Mengerjakan TP Mobpro",
+            "kerjakan sebelum jam praktikum",
+            "2025-05-21 23:59:59"
+        ),
+        Task(
+            5,
+            "Mengerjakan TP Mobpro",
+            "kerjakan sebelum jam praktikum",
+            "2025-05-21 23:59:59"
+        ),
+        Task(
+            6,
+            "Mengerjakan TP Mobpro",
+            "kerjakan sebelum jam praktikum",
+            "2025-05-21 23:59:59"
+        ),
+        Task(
+            7,
+            "Mengerjakan TP Mobpro",
+            "kerjakan sebelum jam praktikum",
+            "2025-05-21 23:59:59"
+        ),
+    )
+}
