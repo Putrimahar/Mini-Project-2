27dbb22b06df94d6a7d72534eacd8c8681205a9e
Putri Maharani <maharani2005.putrii@gmail.com>
2025-05-08 02:38:28 +0700

    Menghapus data dari database

--- app/src/main/java/com/putrimaharani0087/miniproject2/screen/DetailScreen.kt
+++ app/src/main/java/com/putrimaharani0087/miniproject2/screen/DetailScreen.kt
@@ -15,7 +15,10 @@ import androidx.compose.foundation.text.KeyboardOptions
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.automirrored.filled.ArrowBack
 import androidx.compose.material.icons.filled.DateRange
+import androidx.compose.material.icons.filled.MoreVert
 import androidx.compose.material.icons.outlined.Check
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
@@ -114,6 +117,12 @@ fun DetailScreen(navController: NavHostController, id: Long? = null) {
                             tint = Color.Black
                         )
                     }
+                    if (id!= null) {
+                        DeleteAction {
+                            viewModel.delete(id)
+                            navController.popBackStack()
+                        }
+                    }
                 }
             )
         }
@@ -127,6 +136,33 @@ fun DetailScreen(navController: NavHostController, id: Long? = null) {
             onDeadlineChange = { deadline = it },
             modifier = Modifier.padding(padding)
         )
+    }
+}
+
+@Composable
+fun DeleteAction(delete: () -> Unit) {
+    var expanded by remember { mutableStateOf(false) }
+
+    IconButton(onClick = {expanded = true})  {
+        Icon(
+            imageVector = Icons.Filled.MoreVert,
+            contentDescription = stringResource(R.string.lainnya),
+            tint = Color.Black
+        )
+        DropdownMenu(
+            expanded = expanded,
+            onDismissRequest = {expanded = false}
+        ) {
+            DropdownMenuItem(
+                text = {
+                    Text(text = stringResource(R.string.hapus))
+                },
+                onClick = {
+                    expanded = false
+                    delete()
+                }
+            )
+        }
     }
 }
 
--- app/src/main/java/com/putrimaharani0087/miniproject2/screen/DetailViewModel.kt
+++ app/src/main/java/com/putrimaharani0087/miniproject2/screen/DetailViewModel.kt
@@ -35,4 +35,10 @@ class DetailViewModel(private val dao: TaskDao) : ViewModel() {
             dao.update(task)
         }
     }
+
+    fun delete(id: Long) {
+        viewModelScope.launch(Dispatchers.IO) {
+            dao.deleteById(id)
+        }
+    }
 }
